a1a056ed3d8fd241fae8764a556fcec847660d9b
Raphael Arce <raphael.arce@ts.berlin>
2024-08-15 10:45:31 +0200

    feat: design polishing
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/tags.tsx
+++ src/components/tags.tsx
@@ -8,7 +8,7 @@ export function Tags() {
 		<div className="flex justify-center pt-10">
 			<div className="flex flex-col items-center gap-y-2">
 				<div className="text-lg">Filtere mit Tags</div>
-				<div className="flex gap-x-3 w-10/12 flex-wrap gap-y-3 justify-center">
+				<div className="flex gap-x-3 max-w-[55rem] flex-wrap gap-y-3 justify-center">
 					{Array.from(tags).map((tag) => (
 						<TagCheckbox key={tag} tag={tag} />
 					))}
