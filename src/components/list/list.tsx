c6bc458ead549499d19d1cc379651da232dd77fb
Raphael Arce <raphael.arce@ts.berlin>
2024-08-21 15:42:54 +0200

    feat: change order of branches to match contact number order
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -24,67 +24,63 @@ export const List: React.FC = () => {
 			id="listView"
 			className="flex flex-col w-full gap-12 justify-between px-6 md:px-12 my-12"
 		>
-			{branches
-				.map((branch, branchIndex) => (
-					<div key={branch.name} className={`flex flex-col w-full`}>
-						<div
-							className={`flex flex-row justify-between w-full border-b-4 ${borderColors[branchIndex]} `}
+			{branches.map((branch, branchIndex) => (
+				<div key={branch.name} className={`flex flex-col w-full`}>
+					<div
+						className={`flex flex-row justify-between w-full border-b-4 ${borderColors[branchIndex]} `}
+					>
+						<h2 className={`text-3xl leading-loose font-medium `}>
+							{branch.name}
+						</h2>
+						<button
+							className={`${iconColors[branchIndex]}`}
+							onClick={() => {
+								const expanded = [...isExpanded];
+								expanded[branchIndex] = !expanded[branchIndex];
+								setIsExpanded(expanded);
+							}}
 						>
-							<h2 className={`text-3xl leading-loose font-medium `}>
-								{branch.name}
-							</h2>
-							<button
-								className={`${iconColors[branchIndex]}`}
-								onClick={() => {
-									const expanded = [...isExpanded];
-									expanded[branchIndex] = !expanded[branchIndex];
-									setIsExpanded(expanded);
-								}}
-							>
-								{isExpanded[branchIndex] ? <MinusIcon /> : <PlusIcon />}
-							</button>
-						</div>
-						{isExpanded[branchIndex] && (
-							<div
-								className={`flex flex-col justify-between lg:flex-row gap-9`}
-							>
-								{ranges.map((range, rangeIndex) => (
-									<ol
-										className=" w-full flex flex-col pt-4 lg:w-1/4"
-										key={range.name}
-									>
-										<div
-											className={`rounded-full w-fit px-4 py-3 mb-4 ${tagColors[branchIndex][rangeIndex]}
-										${branchIndex === 3 ? "text-white" : "text-sr-lighter-black"}`}
-										>
-											{range.name}
-										</div>
-										{contacts
-											.filter(
-												(contact) =>
-													contact.branch === branch.name &&
-													contact.range === range.name,
-											)
-											.map((contact, contactIndex) => (
-												<li
-													key={contact.organisation}
-													className={`font-medium border-sr-light-grey px-2 py-3 flex flex-row cursor-pointer
-												${contactIndex === 0 ? "border-none" : "border-t"}`}
-													onClick={() => setSelectedContact(contact)}
-												>
-													<div className="min-w-9">
-														{findContactsIndex(contact)}
-													</div>
-													<div>{contact.organisation}</div>
-												</li>
-											))}
-									</ol>
-								))}
-							</div>
-						)}
+							{isExpanded[branchIndex] ? <MinusIcon /> : <PlusIcon />}
+						</button>
 					</div>
-				))
-				.reverse()}
+					{isExpanded[branchIndex] && (
+						<div className={`flex flex-col justify-between lg:flex-row gap-9`}>
+							{ranges.map((range, rangeIndex) => (
+								<ol
+									className=" w-full flex flex-col pt-4 lg:w-1/4"
+									key={range.name}
+								>
+									<div
+										className={`rounded-full w-fit px-4 py-3 mb-4 ${tagColors[branchIndex][rangeIndex]}
+										${branchIndex === 3 ? "text-white" : "text-sr-lighter-black"}`}
+									>
+										{range.name}
+									</div>
+									{contacts
+										.filter(
+											(contact) =>
+												contact.branch === branch.name &&
+												contact.range === range.name,
+										)
+										.map((contact, contactIndex) => (
+											<li
+												key={contact.organisation}
+												className={`font-medium border-sr-light-grey px-2 py-3 flex flex-row cursor-pointer
+												${contactIndex === 0 ? "border-none" : "border-t"}`}
+												onClick={() => setSelectedContact(contact)}
+											>
+												<div className="min-w-9">
+													{findContactsIndex(contact)}
+												</div>
+												<div>{contact.organisation}</div>
+											</li>
+										))}
+								</ol>
+							))}
+						</div>
+					)}
+				</div>
+			))}
 		</div>
 	);
 };
